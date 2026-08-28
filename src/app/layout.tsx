1aa05d9930da7a48bc9486f44ab610bf09635dac
Prabhat Bhusal <prabhatbhusal777@gmail.com>
2026-08-28 21:47:58 +0545

    added improvement

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,10 @@
+import type { Metadata } from "next";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 
-export const metadata = {
+export const metadata: Metadata = {
+  metadataBase: new URL("https://www.bhumluconstruction.com.np"),
   title: "Bhumlu Construction",
   description: "Transmission Line Survey & Construction Company in Nepal",
 };
